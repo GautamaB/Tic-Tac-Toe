290db7c79ad9e4c8c03a3f1f72663cd81a5c63e8
Thib <thibaud.royer051@gmail.com>
2018-08-04 16:41:55 +0200

    add win condition

--- script.js
+++ script.js
@@ -17,11 +17,44 @@ function move(cell) {
 }
 
 function turnDirection() {
-  if (document.turn == "X") {
+  if (winCombos(document.turn)) {
+    setMessage("The player " + document.turn + " has Win!!");
+  } else if (document.turn == "X") {
     document.turn = "O";
+    setMessage("It's " + document.turn + "'s trun.");
   } else {
     document.turn = "X";
+    setMessage("It's " + document.turn + "'s trun.");
   }
+}
+
+function winCombos(move) {
+  var result = false;
+  if (
+    checkRow(0, 1, 2, move) ||
+    checkRow(3, 4, 5, move) ||
+    checkRow(6, 7, 8, move) ||
+    checkRow(0, 3, 6, move) ||
+    checkRow(1, 4, 7, move) ||
+    checkRow(2, 5, 8, move) ||
+    checkRow(0, 4, 8, move) ||
+    checkRow(2, 4, 6, move)
+  ) {
+    result = true;
+  }
+  return result;
+}
+
+function checkRow(a, b, c, move) {
+  var result = false;
+  if (getCell(a) == move && getCell(b) == move && getCell(c) == move) {
+    result = true;
+  }
+  return result;
+}
+
+function getCell(number) {
+  return document.getElementById("s" + number).innerText;
 }
 
 startGame();
