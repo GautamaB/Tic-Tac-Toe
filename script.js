8ef4ce3e28cfc74956090b952623dd79a7d32060
Thib <thibaud.royer051@gmail.com>
2018-08-04 15:50:14 +0200

    meesage begin and block cell

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 function startGame() {
   document.turn = "X";
+  setMessage("The game beging, " + document.turn + " started the first.");
+}
+
+function setMessage(msg) {
+  document.getElementById("gameStatus").innerText = msg;
 }
 
 function move(cell) {
-  cell.innerHTML = document.turn;
-  turnDirection();
+  if (cell.innerText == "") {
+    cell.innerHTML = document.turn;
+    turnDirection();
+  } else {
+    setMessage("Choose another cell !");
+  }
 }
 
 function turnDirection() {
@@ -14,4 +23,5 @@ function turnDirection() {
     document.turn = "X";
   }
 }
+
 startGame();
