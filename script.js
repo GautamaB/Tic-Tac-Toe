f7e99c2d7fdb13194c5d089079c1b00e435b5f41
Thib <thibaud.royer051@gmail.com>
2018-08-06 14:24:36 +0200

    es6

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-function startGame() {
-  for (var i = 0; i <= 8; i++) {
+const startGame = () => {
+  for (let i = 0; i <= 8; i++) {
     clearCell(i);
   }
-  document.turn = "X";
+  document.turn = "DC";
   document.winner = null;
   setMessage("The game beging, " + document.turn + " started the first.");
-}
+};
 
-function setMessage(msg) {
+const setMessage = msg => {
   document.getElementById("gameStatus").innerText = msg;
-}
+};
 
-function move(cell) {
+const move = cell => {
   if (document.winner != null) {
     setMessage(document.turn + " has already win!");
   } else if (cell.innerText == "") {
@@ -20,32 +20,32 @@ function move(cell) {
   } else {
     setMessage("Choose another cell !");
   }
-}
+};
 
-function CheckforTie() {
-  for (var i = 1; i < 9; i++) {
+const CheckforTie = () => {
+  for (let i = 1; i < 9; i++) {
     if (getCell(i) == "") return false;
   }
   return true;
-}
+};
 
-function turnDirection() {
+const turnDirection = () => {
   if (winCombos(document.turn)) {
     setMessage("The player " + document.turn + " has Win!!");
     document.winner = document.turn;
   } else if (CheckforTie()) {
     setMessage("Equality !! Play again.");
-  } else if (document.turn == "X") {
-    document.turn = "O";
+  } else if (document.turn == "DC") {
+    document.turn = "Marvel";
     setMessage("It's " + document.turn + "'s trun.");
   } else {
-    document.turn = "X";
+    document.turn = "DC";
     setMessage("It's " + document.turn + "'s trun.");
   }
-}
+};
 
-function winCombos(move) {
-  var result = false;
+const winCombos = move => {
+  let result = false;
   if (
     checkRow(0, 1, 2, move) ||
     checkRow(3, 4, 5, move) ||
@@ -59,22 +59,22 @@ function winCombos(move) {
     result = true;
   }
   return result;
-}
+};
 
-function checkRow(a, b, c, move) {
-  var result = false;
+const checkRow = (a, b, c, move) => {
+  let result = false;
   if (getCell(a) == move && getCell(b) == move && getCell(c) == move) {
     result = true;
   }
   return result;
-}
+};
 
-function getCell(number) {
+const getCell = number => {
   return document.getElementById("s" + number).innerText;
-}
+};
 
-function clearCell(number) {
+const clearCell = number => {
   document.getElementById("s" + number).innerText = "";
-}
+};
 
 startGame();
